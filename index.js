7e23b6d497e2d4351d2a811876814016dc1746e9
Priscille Raskin <p.raskin@citron.io>
2021-06-13 23:58:44 +0200

    CORS#1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const customerRouter = require('./routes/customer');
-const popcornRouter = require('./routes/popcorn');
-const partnerRouter = require('./routes/partner');
+const customerRouter = require("./routes/customer");
+const popcornRouter = require("./routes/popcorn");
+const partnerRouter = require("./routes/partner");
 
 app.use(express.json());
 app.use(
@@ -12,24 +12,25 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.json({'message': 'ok'});
-})
+app.get("/", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.json({ message: "ok" });
+});
 
-app.use('/customer', customerRouter);
-app.use('/popcorn', popcornRouter);
-app.use('/partner', partnerRouter);
+app.use("/customer", customerRouter);
+app.use("/popcorn", popcornRouter);
+app.use("/partner", partnerRouter);
 
 /* Error handler middleware */
 app.use((err, req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
   const statusCode = err.statusCode || 500;
   console.error(err.message, err.stack);
-  res.status(statusCode).json({'message': err.message});
-
+  res.status(statusCode).json({ message: err.message });
 
   return;
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
