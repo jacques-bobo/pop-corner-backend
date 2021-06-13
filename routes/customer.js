2ac775c0ae8787e2b1676265e337ea0bcafc164e
Priscille Raskin <p.raskin@citron.io>
2021-06-14 00:05:32 +0200

    CORS#3

--- routes/customer.js
+++ routes/customer.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db_manager = require('../services/customer');
-
-
+const db_manager = require("../services/customer");
 
 /* GET customers */
-router.get('/getall/', async function(req, res) {
+router.get("/getall/", async function (req, res) {
   try {
+    res.header("Access-Control-Allow-Origin", "*");
     res.json(await db_manager.getMultiple(req.query.page));
   } catch (err) {
     console.error(`Error while getting customers `, err.message);
@@ -15,9 +14,10 @@ router.get('/getall/', async function(req, res) {
 });
 
 // Get customer from id
-router.get('/', async function(req, res) {
+router.get("/", async function (req, res) {
   var customerID = req.query.id;
   try {
+    res.header("Access-Control-Allow-Origin", "*");
     res.json(await db_manager.getCustomer(customerID));
   } catch (err) {
     console.error(`Error while getting this customer `, err.message);
@@ -26,7 +26,7 @@ router.get('/', async function(req, res) {
 });
 
 // Get customers from city
-router.get('/get_from_city', async function(req, res) {
+router.get("/get_from_city", async function (req, res) {
   var city = req.query.city;
   try {
     res.json(await db_manager.getCustomersByCity(city));
@@ -37,28 +37,44 @@ router.get('/get_from_city', async function(req, res) {
 });
 
 // Add customer
-router.get('/insert', async function(req, res) {
+router.get("/insert", async function (req, res) {
   var username = req.query.username;
   var email = req.query.email;
   var password = req.query.password;
   var city = req.query.city;
   var vegetarian = req.query.vegetarian;
   try {
-    res.json(await db_manager.insertCustomer(username, email, password, city, vegetarian));
+    res.json(
+      await db_manager.insertCustomer(
+        username,
+        email,
+        password,
+        city,
+        vegetarian
+      )
+    );
   } catch (err) {
     console.error(`Error while adding this customers `, err.message);
     next(err);
   }
 });
 
-router.post('/insert', async function(req, res) {
+router.post("/insert", async function (req, res) {
   var username = req.body.username;
   var email = req.body.email;
   var password = req.body.password;
   var city = req.body.city;
   var vegetarian = req.body.vegetarian;
   try {
-    res.json(await db_manager.insertCustomer(username, email, password, city, vegetarian));
+    res.json(
+      await db_manager.insertCustomer(
+        username,
+        email,
+        password,
+        city,
+        vegetarian
+      )
+    );
   } catch (err) {
     console.error(`Error while adding this customers `, err.message);
     next(err);
@@ -66,16 +82,16 @@ router.post('/insert', async function(req, res) {
 });
 
 // Authenticate customer
-router.get('/authenticate', async function(req, res) {
+router.get("/authenticate", async function (req, res) {
   var email = req.query.email;
   var password = req.query.password;
   try {
+    res.header("Access-Control-Allow-Origin", "*");
     res.json(await db_manager.authenticate(email, password));
   } catch (err) {
     console.error(`Error while authenticating this customer `, err.message);
     next(err);
   }
 });
-
 
 module.exports = router;
